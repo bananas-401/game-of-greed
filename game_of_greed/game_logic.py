52efc14c6f20a0d45e1cb51f5d826e22e87cb3c3
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-16 17:05:32 -0800

    created test files and inserted base code

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,24 @@
+class GameLogic:  
+  def __init__(self):
+    pass
+
+  def roll_dice():
+    pass
+
+  def calculate_score():
+    pass
+
+
+class Banker:
+  def __init__(self):
+    pass
+
+  def shelf:
+    pass
+
+  def Bank:
+    pass
+
+  def clear_shelf:
+    pass
+    
