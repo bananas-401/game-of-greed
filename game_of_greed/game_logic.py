ea61fabd4f30df62964eefeedb78e25ad58b9b81
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-11-16 18:04:59 -0800

    finished roll dice funtion

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,24 +1,38 @@
+import random
+
+active_game = True
+
 class GameLogic:  
   def __init__(self):
     pass
 
-  def roll_dice():
-    pass
+  @staticmethod
+  def roll_dice(number_of_dice):
+    result = []
+    for die in range(number_of_dice):
+      result.append(random.randint(1,6))
+    return tuple(result)
 
-  def calculate_score():
-    pass
+  @staticmethod
+  def calculate_score(tuple_input):
+    return 42
 
 
 class Banker:
   def __init__(self):
     pass
 
-  def shelf:
+  def shelf():
     pass
 
-  def Bank:
+  def Bank():
     pass
 
-  def clear_shelf:
+  def clear_shelf():
     pass
-    
+
+current_roll = GameLogic.roll_dice(6)
+print(current_roll)
+
+current_score = GameLogic.calculate_score(current_roll)
+print(current_score)
