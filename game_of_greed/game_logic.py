53fc23f55b339f260dcc1e6e2777af89ee0af5a0
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-17 16:38:26 -0800

    scoring function implemented and passing

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,4 +1,5 @@
 import random
+from collections import Counter
 
 active_game = True
 
@@ -14,8 +15,53 @@ class GameLogic:
     return tuple(result)
 
   @staticmethod
-  def calculate_score(tuple_input):
-    return 42
+  def calculate_score(dice_roll):
+    #reset dice count and points
+    points = 0
+    dice_values = {
+      6: 0,
+      5: 0,
+      4: 0,
+      3: 0,
+      2: 0,
+      1: 0,
+    }
+    
+    #count the dice
+    for value in dice_roll:
+      dice_values[value] += 1
+      
+    #check for straight
+    # if all([count == 1 for count in dice_values.values()]):
+    #   return 1500
+
+    #check for straight
+    temp = Counter(dice_roll)
+    print(temp)
+    print(len(temp))
+    if len(temp) == 6:
+      return 1500
+
+    #check for pairs
+    pair_count = 0
+    for count in dice_values:
+      if dice_values[count] == 2:
+        pair_count += 1
+    if pair_count == 3:
+      return 1500
+
+    #generic matches
+    for count in dice_values:
+      if count != 1 and dice_values[count] > 2:
+        points += count * 100 * (dice_values[count] - 2)
+      elif count == 1:
+        if dice_values[count] < 3:
+          points += 100 * dice_values[count]
+        else:
+          points += 1000 * (dice_values[count]-2)
+      elif count == 5:
+        points += 50 * dice_values[count]
+    return points
 
 
 class Banker:
